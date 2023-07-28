84e94ed6656e0f93cc80914f8919175aeac8f57d
David Arenas <davejs136@gmail.com>
2023-07-28 11:10:09 -0400

    style: apply responsive for homepage features

--- src/layouts/function-components/HomapageFeature.jsx
+++ src/layouts/function-components/HomapageFeature.jsx
@@ -9,7 +9,7 @@ const HomapageFeature = ({ plan_list }) => {
             <h3 className="h4 text-xl lg:text-3xl font-bold relative flex items-center gap-2 mb-4">
               <span className="w-3 h-3 bg-color-1 rounded-full"></span>
               {plan.title}</h3>
-            <ul className="grid grid-cols-3 gap-8">
+            <ul className="grid grid-cols-1 gap-4 md:gap-8 md:grid-cols-3">
               {plan.list.map((item, i) => ((
                 <li className="rounded-lg bg-color-1 text-white p-5 shadow-lg shadow-color-2" key={i}>
                   <h4 className="lg:text-2xl text-white">{item.name}</h4>
